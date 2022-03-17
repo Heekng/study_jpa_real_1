a7dbc60d50177e1e2100a21a7290c4e5851395ca
heekng <rhkd1769@gmail.com>
2022-03-17 22:19:26 +0900

    회원 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        //createQuery: 쿼리(jqpl)와 리턴타입 클래스를 지정하고 getResultList로 리스트를 받아온다.
+        // 멤버의 엔티티에 대해 조회하는 것
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
