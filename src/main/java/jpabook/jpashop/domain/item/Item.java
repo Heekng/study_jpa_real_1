bf307a051da5515fc106f50ebff30f3fc1eccab8
heekng <rhkd1769@gmail.com>
2022-03-19 00:57:20 +0900

    상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,8 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) //JOINED: 정규화된, SINGLE_TABLE: 한 테이블에 다 넣은
 @DiscriminatorColumn(name = "dtype") //구분(자), SINGLE_TABLE 이기에 DB입장에서 구분하기 위해 사용
-@Getter @Setter
+@Getter
+//@Setter
 public abstract class Item {
 
     @Id
@@ -25,4 +27,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStore(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param stockQuantity
+     */
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
