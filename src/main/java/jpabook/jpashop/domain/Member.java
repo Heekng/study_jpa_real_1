a272b74dd98b04ef37d7a873629317d5651d4854
heekng <rhkd1769@gmail.com>
2022-03-19 22:27:40 +0900

    간단한 주문 조회 V1: 엔티티를 직접 노출

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -21,10 +21,11 @@ public class Member {
 
     private String name;
 
-    @JsonIgnore //제외한다. 하지만 화면에 뿌리는 로직이 엔티티에 추가된다.
+//    @JsonIgnore //제외한다. 하지만 화면에 뿌리는 로직이 엔티티에 추가된다.
     @Embedded //내장타입을 포함했다, 한쪽만 존재해도 된다.
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member") //다대일 mappedBy: 나는 이것에 의해 매핑되었다.(읽기전용)
     private List<Order> orders = new ArrayList<>();
 }
